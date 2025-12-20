a01e201b099ed31fe54cbbfcf5e1d1fee3feb3ec
Giuseppe Tutino <giuseppetutino1@gmail.com>
2025-12-20 09:45:29 +0100

    First try to build the CFG

--- src/cfg.c
+++ src/cfg.c
@@ -3,45 +3,20 @@
 #include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <assert.h>
 
 struct CFG_Ctx {
     CFG *cfg;
     Parser *parser;
 };
 
-static size_t cfg_print_points_impl(AST_Node *node, size_t counter) {
-    switch (node->type) {
-    case NODE_ASSIGN:
-    case NODE_SKIP:
-        printf("(P%zu)\n", counter++);
-        parser_print_ast(node);
-        break;
-    case NODE_SEQ:
-        counter = cfg_print_points_impl(node->as.child.left, counter);
-        counter = cfg_print_points_impl(node->as.child.right, counter);
-        break;
-    case NODE_IF:
-        printf("if (P%zu) b then\n", counter++);
-        counter = cfg_print_points_impl(node->as.child.left, counter);
-        printf("(P%zu) else\n", counter++);
-        counter = cfg_print_points_impl(node->as.child.right, counter);
-        printf("fi\n");
-        break;
-    case NODE_WHILE:
-        printf("while (P%zu) b do\n", counter++);
-        counter = cfg_print_points_impl(node->as.child.left, counter);
-        printf("(P%zu) done\n", counter++);
-        break;
-    default:
-        break;
-    }
-    return counter;
-}
+static CFG_Node build_node(size_t id) {
+    CFG_Node node = {
+        .id = id,
+        .edge_count = 0,
+    };
 
-static size_t cfg_print_points(AST_Node *node) {
-    size_t count = cfg_print_points_impl(node, 1);
-    printf("(P%zu)\n", count);
-    return count;
+    return node;
 }
 
 static size_t count_nodes(AST_Node *node, size_t counter) {
@@ -60,7 +35,7 @@ static size_t count_nodes(AST_Node *node, size_t counter) {
         counter = count_nodes(node->as.child.right, counter);
         break;
     case NODE_WHILE:
-        counter += 2;
+        counter += 1;
         counter = count_nodes(node->as.child.left, counter);
         break;
     default:
@@ -69,8 +44,117 @@ static size_t count_nodes(AST_Node *node, size_t counter) {
     return counter;
 }
 
-static CFG *build_cfg(AST_Node *node, size_t counter) {
-    /* TODO */
+
+static size_t build_cfg_impl(CFG *cfg, AST_Node *node, size_t counter) {
+    switch (node->type) {
+    case NODE_SKIP:
+        /* TODO: like the assign */
+        break;
+    case NODE_ASSIGN:
+
+        /* Create the assign node */
+        cfg->nodes[counter] = build_node(counter);
+        cfg->nodes[counter].edges[0].src = counter;
+        cfg->nodes[counter].edges[0].dst = -1;
+        cfg->nodes[counter].edges[0].type = EDGE_ASSIGN;
+        cfg->nodes[counter].edges[0].as.assign = node;
+
+        /* TODO: FIND A WAY TO GET THE PREDECESSOR OF A NODE */
+
+        /* If there is a predecessor node we need to wire an edge to this new node */
+        if (counter > 0) {
+            // size_t prev_node_edge_count = cfg->nodes[counter-1].edge_count;
+            // if (prev_node_edge_count == 2) {
+            //     assert(0 && "UNREACHABLE");
+            // }
+
+            // /* While end case */
+            // if (prev_node_edge_count == 1) {
+            //     /* Look for the loop invariant node */
+            //     for (size_t i = counter-1; i != 0; --i) {
+            //         if (cfg->nodes[i].edges[1].type == EDGE_GUARD) {
+            //             cfg->nodes[i].edges[1].dst = counter;
+            //             cfg->nodes[i].edge_count++;
+            //             counter++;
+            //             break;
+            //         }
+            //     }
+            // } else {
+            //     cfg->nodes[counter-1].edges[prev_node_edge_count].dst = counter;
+            //     cfg->nodes[counter-1].edge_count++;
+            // }
+        }
+
+        counter++;
+        break;
+    case NODE_SEQ:
+        counter = build_cfg_impl(cfg, node->as.child.left, counter);
+        counter = build_cfg_impl(cfg, node->as.child.right, counter);
+        break;
+    case NODE_IF:
+        // printf("if (P%zu) b then\n", counter++);
+        // counter = build_cfg_impl(cfg, node->as.child.left, counter);
+        // printf("(P%zu) else\n", counter++);
+        // counter = build_cfg_impl(cfg, node->as.child.right, counter);
+        // printf("fi\n");
+        break;
+    case NODE_WHILE:
+        /* Loop invariant Node */
+        const size_t loop_inv = counter;
+        cfg->nodes[counter] = build_node(counter);
+
+        cfg->nodes[counter].edges[0].src = counter;
+        cfg->nodes[counter].edges[0].dst = -1;
+        cfg->nodes[counter].edges[0].type = EDGE_GUARD;
+        cfg->nodes[counter].edges[0].as.guard.condition = node;
+        cfg->nodes[counter].edges[0].as.guard.val = true;
+
+        cfg->nodes[counter].edges[1].src = counter;
+        cfg->nodes[counter].edges[1].dst = -1;
+        cfg->nodes[counter].edges[1].type = EDGE_GUARD;
+        cfg->nodes[counter].edges[1].as.guard.condition = node;
+        cfg->nodes[counter].edges[1].as.guard.val = false;
+
+        /* TODO: see ASSIGN case */
+        /* If there is a predecessor node we need to wire an edge to this new node */
+        if (counter > 0) {
+            // size_t prev_node_edge_count = cfg->nodes[counter-1].edge_count;
+            // if (prev_node_edge_count == 2) {
+            //     assert(0 && "UNREACHABLE");
+            // }
+
+            // cfg->nodes[counter-1].edges[prev_node_edge_count].dst = counter;
+            // cfg->nodes[counter-1].edge_count++;
+        }
+
+        counter++;
+
+        counter = build_cfg_impl(cfg, node->as.child.left, counter);
+
+        /* Wire the last added node to the loop invariant node */
+        cfg->nodes[counter-1].edges[0].dst = loop_inv;
+        cfg->nodes[counter-1].edge_count++;
+        break;
+    default:
+        break;
+    }
+    return counter;
+}
+
+static void build_cfg(CFG *cfg, AST_Node *root) {
+    size_t counter = build_cfg_impl(cfg, root, 0);
+
+    /* TODO: Create exit node and wire to the last node */
+}
+
+void cfg_print(CFG *cfg) {
+    for (size_t i = 0; i < cfg->count; ++i) {
+        CFG_Node node = cfg->nodes[i];
+        printf("\n(NODE %zu)", i);
+        for (size_t j = 0; j < node.edge_count; ++j) {
+            printf("\n\t[%zu -> %zu] type = %d", node.edges[j].src, node.edges[j].dst, node.edges[j].type);
+        }
+    }
 }
 
 CFG_Ctx *cfg_init(const char *src) {
@@ -92,7 +176,8 @@ CFG *cfg_get(CFG_Ctx *ctx) {
         ctx->cfg = xmalloc(sizeof(CFG));
         ctx->cfg->count = count_nodes(root, 1);
         ctx->cfg->nodes = xmalloc(sizeof(CFG_Node)*(ctx->cfg->count));
-        ctx->cfg = build_cfg(root, 1);
+        build_cfg(ctx->cfg, root);
+        cfg_print(ctx->cfg);
     }
 
     return ctx->cfg;
