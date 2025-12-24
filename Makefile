346a47d57d9dfd0c45c06acb26b1c2563b578bb7
Giuseppe Tutino <giuseppetutino1@gmail.com>
2025-12-24 12:40:01 +0100

    Adding tests for interval_create and interval_leq

--- Makefile
+++ Makefile
@@ -1,7 +1,14 @@
 CC=gcc
 CFLAGS=-Wall -Wextra -std=c99 -pedantic -Werror=vla -ggdb
 
-all: main
+all: main test
 
 main: main.c src/lang/lexer.c src/utils.c src/lang/parser.c src/lang/cfg.c src/domain/abstract_interval_domain.c src/abstract_analyzer.c
 	$(CC) $(CFLAGS) main.c src/lang/lexer.c src/utils.c src/lang/parser.c src/lang/cfg.c src/domain/abstract_interval_domain.c src/abstract_analyzer.c -o main
+
+test: abstract_interval_domain_test
+
+abstract_interval_domain_test: test/abstract_interval_domain_test.c
+	$(CC) $(CFLAGS) test/abstract_interval_domain_test.c -o test/abstract_interval_domain_test
+	./test/abstract_interval_domain_test
+	rm ./test/abstract_interval_domain_test
---
+++ test/abstract_interval_domain_test.c
@@ -0,0 +1,113 @@
+#include "../src/domain/abstract_interval_domain.c"
+#include <assert.h>
+#include <stdio.h>
+
+void interval_create_test(void) {
+    /* m,n integers */
+    int64_t m = -10;
+    int64_t n = 10;
+    Interval i = {0};
+
+    i = interval_create(m, n, 3, 2);
+    assert(i.type == INTERVAL_BOTTOM);
+
+    i = interval_create(m, n, INTERVAL_MIN_INF, INTERVAL_PLUS_INF);
+    assert(i.type == INTERVAL_STD);
+
+    i = interval_create(m, n, 2, 2);
+    assert(i.type == INTERVAL_STD && i.a == i.b && i.a == 2);
+
+    i = interval_create(m, n, -11, -11);
+    assert(i.type == INTERVAL_STD && i.a == i.b && i.a == -11);
+
+    i = interval_create(m, n, 11, 11);
+    assert(i.type == INTERVAL_STD && i.a == i.b && i.a == 11);
+
+    i = interval_create(m, n, INTERVAL_MIN_INF, 9);
+    assert(i.type == INTERVAL_STD);
+
+    i = interval_create(m, n, 9, INTERVAL_PLUS_INF);
+    assert(i.type == INTERVAL_STD);
+
+    i = interval_create(m, n, -6, 7);
+    assert(i.type == INTERVAL_STD);
+
+    i = interval_create(m, n, -20, 7);
+    assert(i.type == INTERVAL_STD && i.a == INTERVAL_MIN_INF && i.b == INTERVAL_PLUS_INF);
+
+    /* m,n infinite */
+    m = INTERVAL_MIN_INF;
+    n = INTERVAL_PLUS_INF;
+
+    i = interval_create(m, n, 3, 2);
+    assert(i.type == INTERVAL_BOTTOM);
+
+    i = interval_create(m, n, INTERVAL_MIN_INF, INTERVAL_PLUS_INF);
+    assert(i.type == INTERVAL_STD);
+
+    i = interval_create(m, n, 2, 2);
+    assert(i.type == INTERVAL_STD && i.a == i.b && i.a == 2);
+
+    i = interval_create(m, n, -11, -11);
+    assert(i.type == INTERVAL_STD && i.a == i.b && i.a == -11);
+
+    i = interval_create(m, n, 11, 11);
+    assert(i.type == INTERVAL_STD && i.a == i.b && i.a == 11);
+
+    i = interval_create(m, n, INTERVAL_MIN_INF, 9);
+    assert(i.type == INTERVAL_STD);
+
+    i = interval_create(m, n, 9, INTERVAL_PLUS_INF);
+    assert(i.type == INTERVAL_STD);
+
+    i = interval_create(m, n, -6, 7);
+    assert(i.type == INTERVAL_STD);
+}
+
+void interval_leq_test(void) {
+    /* STD intervals */
+    Interval i1 = {
+        .type = INTERVAL_STD,
+        .a = 10,
+        .b = 12,
+    };
+
+    Interval i2 = {
+        .type = INTERVAL_STD,
+        .a = 10,
+        .b = 20,
+    };
+
+    assert(interval_leq(i1, i2) == true);
+
+    /* i1 and i2 BOTTOM */
+    i1.type = INTERVAL_BOTTOM;
+    i2.type = INTERVAL_BOTTOM;
+    assert(interval_leq(i1, i2));
+
+    /* i1 BOTTOM */
+    i1.type = INTERVAL_BOTTOM;
+    i2 = (Interval) {
+        .type = INTERVAL_STD,
+        .a = 10,
+        .b = 20,
+    };
+    assert(interval_leq(i1, i2) == true);
+
+    /* i2 BOTTOM */
+    i1 = (Interval) {
+        .type = INTERVAL_STD,
+        .a = 10,
+        .b = 20,
+    };
+    i2.type = INTERVAL_BOTTOM;
+    assert(interval_leq(i1, i2) == false);
+}
+
+
+int main(void) {
+    interval_leq_test();
+    printf("[OK]: interval_leq_test pass.\n");
+    interval_create_test();
+    printf("[OK]: interval_create_test pass.\n");
+}
